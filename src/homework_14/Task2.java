504dce04863cce6565f1dcf8b66a7f77d31a4ade
Dmitriy Push <777pdn17@gmail.com>
2024-09-28 00:16:00 +0200

    src/homework_14/Task2.java

--- src/homework_14/Task2.java
+++ src/homework_14/Task2.java
@@ -1,4 +1,13 @@
 package homework_14;
 
 public class Task2 {
+    public static int sumOfEvenNumbers(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                sum += array[i];
+            }
+        }
+        return sum;
+    }
 }
